4ca36f6479d9e75d776919daae09814082cbb73c
rosemolt <rosemolthomas02@gmail.com>
2024-06-10 05:22:10 -0700

    API created

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express=require("express")
 const mongoose=require("mongoose")
 const cors=require("cors")
+const student=require("./models/student")
 
 const app=express()
 app.use(cors())
@@ -9,10 +10,10 @@ app.get("/",(req,res)=>{
     res.send("hello")
 })
 
-app.get("/contact",(req,res)=>{
+app.post("/contact",(req,res)=>{
     res.send("Welcome to my contact page")
 })
 
-app.listen(8081,()=>{
+app.listen(8083,()=>{
     console.log("server started")
 })
---
+++ models/student.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose")
+const schema = mongoose.Schema(
+    {
+        "name":String,
+        "rollNo":String,
+        "admNo":String,
+        "college":String
+    }
+)
+
+let studentmodel=mongoose.Model("students",schema)
+module.exports={studentmodel}

07935617f8e7c60a7a97b46660b270708b2995b5
rosemolt <rosemolthomas02@gmail.com>
2024-06-10 04:40:51 -0700

    API created

---
+++ app.js
@@ -0,0 +1,18 @@
+const express=require("express")
+const mongoose=require("mongoose")
+const cors=require("cors")
+
+const app=express()
+app.use(cors())
+
+app.get("/",(req,res)=>{
+    res.send("hello")
+})
+
+app.get("/contact",(req,res)=>{
+    res.send("Welcome to my contact page")
+})
+
+app.listen(8081,()=>{
+    console.log("server started")
+})
